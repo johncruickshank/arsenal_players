004275514aa5617f13528ea2670727f09df4737e
John Cruickshank <johncruickshank87@gmail.com>
2017-12-12 15:07:24 +0000

    adds server setup data

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
 app.use(express.static("client/build"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+var MongoClient = require("mongodb").MongoClient;
+
+MongoClient.connect("mongodb://localhost:27017/arsenal_players", function(err, database) {
+  if (err) {
+    return console.log(err);
+  }
+
+  // golbal db variable
+  db = database;
+  console.log("Connected to DB");
+
+  app.listen(3000, function(){
+    console.log("Listening on port 3000");
+  });
+});
 
 app.get("/", function(req, res){
 	res.sendFile(__dirname + "/client/build/index.html");
-});
-
-app.listen(3000, function(){
-	console.log("Listening");
 });
